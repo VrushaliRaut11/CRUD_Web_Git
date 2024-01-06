11da9dca470526082fda8db6d10c77e63f4f08e7
VrushaliRaut11 <vrushalikudale93@gmail.com>
2024-01-06 16:35:43 +0530

    save

--- Crud_web_git_Example/src/main/java/com/cjc/serviceImpl/StudentServiceImpl.java
+++ Crud_web_git_Example/src/main/java/com/cjc/serviceImpl/StudentServiceImpl.java
@@ -1,14 +1,20 @@
 package com.cjc.serviceImpl;
 
-import com.cjc.model.Student;
-import com.cjc.serviceI.StudentServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.cjc.model.Student;
+import com.cjc.repository.StudentRepository;
+import com.cjc.serviceI.StudentServiceI;
+@Service
 public class StudentServiceImpl implements StudentServiceI {
 
+	@Autowired
+	StudentRepository sr;
 	@Override
 	public void saveStudent(Student s) {
 		// TODO Auto-generated method stub
-		
+		sr.save(s);
 	}
 
 }
